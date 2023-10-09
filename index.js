818540ed7c2d0ee71177d72da856b22f0dcb0a5c
Anna <kowalewskajaanna@gmail.com>
2023-10-09 23:47:20 +0200

    fix:add steps

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const memoryCards = document.querySelectorAll('.memory__card');
 let hasFlippedCard = false;
 let lockBoard = false;
 let firstCard, secondCard;
+let steps = 0;
 memoryCards.forEach(card => card.addEventListener('click', flipCard));
+
 
 
 
@@ -14,21 +16,27 @@ memoryCards.forEach(card => card.addEventListener('click', flipCard));
 function flipCard() {
     if (lockBoard) return;
     if (this === firstCard) return;
+
     this.classList.add('flip__card');
+
     if (!hasFlippedCard) {
         hasFlippedCard = true;
         firstCard = this;
-
-    } else {
-        secondCard = this;
-        checkForMatchCard();
+        return;
     }
 
+    secondCard = this;
+    lockBoard = true;
+
+    checkForMatchCard();
 }
 
 
 function checkForMatchCard() {
-    let isMatch = firstCard.dataset.cardName === secondCard.dataset.cardName;
+    let isMatch = firstCard.dataset.cardname === secondCard.dataset.cardname;
+    steps++;
+console.log(steps);
+    console.log(firstCard.dataset.cardname, secondCard.dataset.cardname);
     isMatch ? disableCards() : unflipCards();
 }
 
